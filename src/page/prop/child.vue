7521a5e6155dc70d491b8ef1b77c69355530ea4b
gisealine <suyiben@126.com>
2022-07-07 23:46:50 +0800

    测试

---
+++ src/page/prop/child.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+
+    <div>
+      <button type="button" @click="changeData">按钮</button>
+
+    </div>
+    <div>
+      {{JSON.stringify(obj)}}
+    </div>
+<!--    <div>
+      {{obj[1].children[0].label}}
+    </div>-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['obj'],
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    changeData() {
+      // this.$set(this.obj, 1, 'abc')
+      // let newObj = Object.assign([], this.obj)
+      // this.obj = [1, 2]
+      this.obj = [
+        {
+          label: 'outter 01',
+          children: [
+            {
+              label: 'inner 01'
+            },
+            {
+              label: 'inner 01'
+            }
+          ]
+        },
+        {
+          label: 'outter 02',
+          children: [
+            {
+              label: 'inner 01'
+            },
+            {
+              label: 'inner 01'
+            }
+          ]
+        }
+      ]
+      this.obj[1].children[0] = {a: 'aaaa'}
+      this.$emit('updateObj', this.obj)
+    }
+  }
+
+}
+</script>
---
+++ src/page/prop/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{JSON.stringify(pobj)}}
+    <Child :obj="pobj" @updateObj="updateObj" >
+
+    </Child>
+  </div>
+</template>
+
+<script>
+import Child from './child'
+export default {
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      pobj: []
+    }
+  },
+  methods: {
+    updateObj(data) {
+      // this.pobj = JSON.parse(JSON.stringify(data))
+      // this.pobj = data
+      // this.$set(this.pobj, data)
+      this.pobj = Object.assign([], data)
+    }
+  }
+}
+</script>
